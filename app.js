18b478783fa278ea058c94c025ffe82b71f725c3
thibautHolberton <thibautbernard@sfr.fr>
2021-05-21 10:11:35 +0200

    feat(): create correction for each task of a project

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const request = require('request');
 
 
 
-var test;
+const urlAuth = 'url'
+const apiKey = 'apiKey'
+const email = 'email'
+const password = "password"
 var fetch = require('node-fetch');
 
 /** Request api holberton to get the api key and return promise of the response**/
@@ -14,12 +16,15 @@ async function requestAuth(){
             body: JSON.stringify({"api_key": apiKey, "email": email, "password": password, "scope": "checker"})
         });
     let result = await response.json()
+    if (result.error){
+        console.log(result.error)
+    }
     return  result.auth_token
 }
 /** Request api holberton to get all tasks of a project and return promise of all tasks */
 async function requestProject(){
     let authToken = await requestAuth();
-    let idProject = 314;
+    let idProject = 269; //305
     let url = 'https://intranet.hbtn.io/projects/' + idProject + '.json?auth_token=' + authToken;
     let response = await fetch(url, {method: 'GET', headers: { 'Content-Type': 'application/json'}});
     let result = await response.json();
@@ -43,11 +48,11 @@ async function parseTasks(){
             dictTasksInfo['checker_available'] = tasks[key].checker_available
             dictTasksInfo['github_dir'] = tasks[key].github_dir
             dictTasksInfo['github_repo'] = tasks[key].github_repo
+            dictTasksInfo['position'] = tasks[key].position
             dictTasks[tasks[key].id] = dictTasksInfo
         }
     }
-    console.log(dictTasks);
-
+    
     return dictTasks;
 }
 
@@ -56,7 +61,10 @@ async function askCorrection(taskId){
     let url = 'https://intranet.hbtn.io/tasks/' + taskId + '/start_correction.json?auth_token=' + authToken;
     let response = await fetch(url, {method: 'POST', headers: { 'Content-Type': 'application/json'}});
     let result = await response.json();
-    console.log(result)
+    if (result.error){
+        console.log(result.error)
+        return result
+    }
     return result.id
 }
 
@@ -65,23 +73,41 @@ async function checkCorrection(idCorrection){
     let url = 'https://intranet.hbtn.io/correction_requests/' + await idCorrection + '.json?auth_token=' + authToken;
     let response = await fetch(url, {method: 'GET', headers: { 'Content-Type': 'application/json'}});
     let result = await response.json();
+    if (result.error){
+        console.log(result.error)
+    }
     return result;
 }
-parseTasks().then(async (dict) => { 
 
-    var idCorrection = await askCorrection(1864);
-    //var idCorrection = 5021192;
-    //console.log(5021192)
-    console.log(idCorrection);
-    var result = await checkCorrection(idCorrection)
-    console.log(result); 
-    // if (result.status === 'Sent'){
-    //     await new Promise(resolve => setTimeout(resolve, 5000));
-    //     var r = await checkCorrection(idCorrection)
-    //     console.log(r)
-    // }else {
-    //     console.log('oula');
-    // };
+async function ProjectCorrection(){
+    var dictTasks = await parseTasks()
 
-});
-
+    /** Ask a correction for each task of the project */
+    for (const [key, value] of Object.entries(dictTasks)){
+        let idCorrection = await askCorrection(key);
+        if (idCorrection.error){
+            return;
+        }
+        let tmpDict = dictTasks[key]
+        tmpDict['idCorrection'] = idCorrection
+        dictTasks[key] = tmpDict
+        console.log('🚀 Task (' + value['github_file'] + ') correction asked')
+    }
+    console.log('⏳ Keep waiting...')
+    await new Promise(resolve => setTimeout(resolve, 20000));
+    console.log('⌛️ Correction done')
+    /** Check the status of the correction for each task of the project */
+    for (const [key, value] of Object.entries(dictTasks)){
+        const result = await checkCorrection(value['idCorrection']);
+        if (result.error){
+            console.log(result.error)
+            return;
+        }
+        if (result.result_display.all_passed === false){
+            console.log(' 🚨 One check or plus have failed on the task ' + value['position'] + ' (' + value['github_file'] + ').')
+            return;
+        }
+    }
+    console.log(Object.keys(dictTasks).length + '/' + Object.keys(dictTasks).length + ' checked! ✅')
+};
+xd();
